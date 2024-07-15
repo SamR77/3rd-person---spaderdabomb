62f6108131cc9ff97125e94d974d412a861c7b5c
Sam Robichaud <Snrobichaud@gmail.com>
2024-07-15 12:41:47 -0300

    Completed Tutorial #06

--- Assets/Player Controller/Scripts/PlayerController.cs
+++ Assets/Player Controller/Scripts/PlayerController.cs
@@ -26,16 +26,19 @@ namespace SamR
         public float runAcceleration = 35f;
         public float runSpeed = 4f;
         public float sprintAcceleration = 50f;
+        public float inAirAcceleration = 25f;
         public float sprintSpeed = 7f;
         public float drag = 20f;
         public float gravity = 25f;
-        public float jumpSpeed = 0.8f;
+        public float jumpSpeed = 1.0f;
         public float movingThreshold = 0.01f;
 
         [Header("Animation")]
         public float playerModelRotationSpeed = 10.0f;
         public float rotateToTargetTime = 0.25f;
 
+        [Header("Environment Details")]
+        [SerializeField] private LayerMask groundLayers;
 
         [Header("Camera Settings")]
         public float lookSenseH = 0.1f;
@@ -48,9 +51,14 @@ namespace SamR
         private Vector2 cameraRotation = Vector2.zero;
         private Vector2 playerTargetRotation = Vector2.zero;
 
+        private bool jumpedLastFrame = false;
         private bool isRotatingClockwise = false;
         private float rotatingToTargetTimer = 0f;
         private float verticalVelocity = 0f;
+        private float antiBump;
+        private float stepOffset;
+
+        private PlayerMovementState lastMovementState = PlayerMovementState.Falling;
         
         
         
@@ -62,9 +70,12 @@ namespace SamR
         #region Startup 
         private void Awake()
         {
-            characterController = GetComponent<CharacterController>();
+            //characterController = GetComponent<CharacterController>();
             playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
             playerState = GetComponent<PlayerState>();
+
+            antiBump = sprintSpeed;
+            stepOffset = characterController.stepOffset;
             
         }
         #endregion
@@ -83,6 +94,8 @@ namespace SamR
 
         private void UpdateMovementState()
         {
+            lastMovementState = playerState.CurrentPlayerMovementState;
+
             bool canRun = CanRun();
             bool isMovementInput = playerLocomotionInput.MovementInput != Vector2.zero;                                     // Order of these operations matters...
             bool isMovingLaterally = IsMovingLaterally();                                                                   // Order of these operations matters...
@@ -97,33 +110,51 @@ namespace SamR
 
             playerState.SetPlayerMovementState(lateralState);
 
-            // Control Airborn State
-            if (isGrounded == false && characterController.velocity.y > 0f)
+            // Control Airborne State
+            if ((isGrounded == false || jumpedLastFrame == true) && characterController.velocity.y > 0f)
             {
                 playerState.SetPlayerMovementState(PlayerMovementState.Jumping);
+                jumpedLastFrame = false;
+                characterController.stepOffset = 0f;
             }
-            else if (isGrounded = false && characterController.velocity.y <= 0f)
+
+            else if ((isGrounded == false || jumpedLastFrame == true) && characterController.velocity.y <= 0f)
             {
                 playerState.SetPlayerMovementState(PlayerMovementState.Falling);
+                jumpedLastFrame = false;
+                characterController.stepOffset = 0f;
+            }
+            else
+            {
+                characterController.stepOffset = stepOffset;
             }
         }
         private void HandleVerticalMovement()
         {
             bool isGrounded = playerState.InGroundedState();
 
-            if (isGrounded && verticalVelocity < 0f)
-            {
-                verticalVelocity = 0f;
-            }
-
             verticalVelocity -= gravity * Time.deltaTime;
+
+            if (isGrounded && verticalVelocity < 0)
+            {
+                verticalVelocity = -antiBump;
+            }
 
             if (playerLocomotionInput.JumpPressed && isGrounded)
             {
-                verticalVelocity += MathF.Sqrt(jumpSpeed * 3 * gravity);
+                verticalVelocity += Mathf.Sqrt(jumpSpeed * 3 * gravity);
+                jumpedLastFrame = true;
             }
-        }
 
+            if (playerState.IsStateGroundedState(lastMovementState) && isGrounded == false)                
+            {
+                verticalVelocity += antiBump;
+            }
+
+            //Clamp terminal Velocity
+         
+
+        }
         private void HandleLateralMovement()
         {
             // Create a quick reference to the player's current state
@@ -132,13 +163,15 @@ namespace SamR
             bool isWalking = playerState.CurrentPlayerMovementState == PlayerMovementState.Walking;
 
             // State dependant acceleration and speed
-            float lateralAcceleration = isWalking == true ? walkAcceleration :
-                                        isSprinting == true ? sprintAcceleration :
-                                        runAcceleration;
+            float lateralAcceleration = isGrounded  == false ? inAirAcceleration :
+                                        isWalking   == true  ? walkAcceleration :
+                                        isSprinting == true  ? sprintAcceleration :
+                                                              runAcceleration;
 
-            float clampLateralMagnitude = isWalking == true   ? walkSpeed :
-                                          isSprinting == true ? sprintSpeed :
-                                          runSpeed;
+            float clampLateralMagnitude = isGrounded  == false ? sprintSpeed : 
+                                          isWalking   == true  ? walkSpeed :
+                                          isSprinting == true  ? sprintSpeed :
+                                                                 runSpeed;
             
 
             Vector3 cameraForwardXZ = new Vector3(playerCamera.transform.forward.x, 0, playerCamera.transform.forward.z).normalized;
@@ -151,12 +184,30 @@ namespace SamR
             // add drag to player
             Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
             newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
-            newVelocity = Vector3.ClampMagnitude(newVelocity, clampLateralMagnitude);
+            newVelocity = Vector3.ClampMagnitude(new Vector3(newVelocity.x, 0f, newVelocity.z),  clampLateralMagnitude);
             newVelocity.y += verticalVelocity;
+            newVelocity = isGrounded == false ? HandleSteepWalls(newVelocity) : newVelocity;
+
 
             // Move Character (unity Suggests only calling this once per frame)
             characterController.Move(newVelocity * Time.deltaTime);
         }
+
+        private Vector3 HandleSteepWalls(Vector3 velocity)
+        { 
+            Vector3 normal = CharacterControllerUtils.GetNormalWithSphereCast(characterController, groundLayers);
+            float angle = Vector3.Angle(normal, Vector3.up);
+            bool validAngle = angle <= characterController.slopeLimit;
+        
+            if(validAngle == false && verticalVelocity < 0f)
+            {
+                velocity = Vector3.ProjectOnPlane(velocity, normal);
+            }
+
+            return velocity;
+        }
+
+
         #endregion
 
         #region Late Update Logic
@@ -168,7 +219,7 @@ namespace SamR
         private void UpdateCameraRotation()
         {
             cameraRotation.x += lookSenseH * playerLocomotionInput.LookInput.x;
-            cameraRotation.y = Mathf.Clamp(cameraRotation.y - lookSenseV * playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
+            cameraRotation.y = Mathf.Clamp(cameraRotation.y - lookSenseV * playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);                    
 
             playerTargetRotation.x += transform.eulerAngles.x + lookSenseH * playerLocomotionInput.LookInput.x;
                                     
@@ -194,6 +245,8 @@ namespace SamR
             Vector3 crossProduct = Vector3.Cross(transform.forward, camForwardProjectedXZ);
             float sign = Mathf.Sign(Vector3.Dot(crossProduct, transform.up));
             RotationMismatch = sign * Vector3.Angle(transform.forward, camForwardProjectedXZ);
+
+            
         }
 
         private void UpdateIdleRotation(float rotationTolerance)
@@ -231,9 +284,33 @@ namespace SamR
         }
         private bool IsGrounded()
         {
-            return characterController.isGrounded;
+            bool grounded = playerState.InGroundedState() ? IsGroundedWhileGrounded() : IsGroundedWhileAirborne();
+
+            return grounded;
         }
 
+        private bool IsGroundedWhileGrounded()
+        {
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - characterController.radius, transform.position.z);
+
+            bool grounded = Physics.CheckSphere(spherePosition, characterController.radius, groundLayers, QueryTriggerInteraction.Ignore);
+
+            return grounded == true;            
+        }
+
+        private bool IsGroundedWhileAirborne()
+        {
+            Vector3 normal = CharacterControllerUtils.GetNormalWithSphereCast(characterController, groundLayers);
+            float angle = Vector3.Angle(normal, Vector3.up);
+            print(angle);
+            bool validAngle = angle <= characterController.slopeLimit;
+
+            return characterController.isGrounded && validAngle;
+        }
+
+       
+
+            
         private bool CanRun()
         { 
         // Returns true if the player is moving forward or diagonally (45 degrees) (strafing or backwards movement will return false)
